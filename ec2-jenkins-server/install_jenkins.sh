20e33e00b127fe9172bdab97a0eb0f1ef786f0b5
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2023-03-29 03:14:23 +0000

    Typos Changes

--- ec2-jenkins-server/install_jenkins.sh
+++ ec2-jenkins-server/install_jenkins.sh
@@ -6,5 +6,5 @@ rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade -y
 amazon-linux-extras install java-openjdk11 -y
 yum install jenkins -y
-systemctl enable jenkin
+systemctl enable jenkins
 systemctl start jenkins
