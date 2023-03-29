47cb44fb46dccb1afd1087ae47b40efbba83555e
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2023-03-29 03:22:34 +0000

    Updated Script

--- ec2-jenkins-server/install_jenkins.sh
+++ ec2-jenkins-server/install_jenkins.sh
@@ -4,7 +4,7 @@ wget -O /etc/yum.repos.d/jenkins.repo \
 https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade -y
-amazon-linux-extras install java-openjdk11 -y
+install java-openjdk11 -y
 yum install jenkins -y
 systemctl enable jenkins
 systemctl start jenkins
