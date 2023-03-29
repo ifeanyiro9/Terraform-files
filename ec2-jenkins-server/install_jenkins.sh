d42e7668c0124b309ea132435fce8c4a00d4eed0
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2023-03-29 04:57:36 +0000

    Updated script

--- ec2-jenkins-server/install_jenkins.sh
+++ ec2-jenkins-server/install_jenkins.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-yum update –y
-wget -O /etc/yum.repos.d/jenkins.repo \
-https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum upgrade -y
-yum install java-openjdk11 -y
+yum install java-11-openjdk-devel -y
+yum install wget -y
+yum install firewalld -y
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install jenkins -y
 systemctl enable jenkins
 systemctl start jenkins
+systemctl start firewalld
+systemctl enable firewalld
+firewall-cmd --add-port=8080/tcp --permanent
+firewall-cmd --reload
